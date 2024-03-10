40a6152cd1afdb55da0e15dea66b1b3de26f1010
Steve De La Rosa <stevedelarosa1036@gmail.com>
2024-03-10 18:56:38 -0400

    completed routes

--- server/routes.js
+++ server/routes.js
@@ -11,6 +11,7 @@ const {
     destroyFavorite,
 } = require("./db");
 
+// GET - Returns an array of users
 router.get("/users", async (req, res, next) => {
     try {
         res.send(await fetchUsers());
@@ -19,4 +20,74 @@ router.get("/users", async (req, res, next) => {
     }
 });
 
+// GET - Returns an array of products
+router.get("/products", async (req, res, next) => {
+    try {
+        res.send(await fetchProducts());
+    } catch (error) {
+        next(error);
+    }
+});
+
+// GET - Returns an array of products
+router.get("/favoriteproducts", async (req, res, next) => {
+    try {
+        res.send(await fetchFavorites());
+    } catch (error) {
+        next(error);
+    }
+});
+
+// POST - Create a new user
+router.post("/signup", async (req, res, next) => {
+    try {
+        const { username, password } = req.body;
+
+        const newUser = await createUser({ username, password });
+
+        res.status(201).send(newUser);
+    } catch (error) {
+        next(error);
+    }
+});
+
+// POST - Create a new product
+router.post("/createproduct", async (req, res, next) => {
+    try {
+        const { name } = req.body;
+
+        const newProduct = await createProduct({ name });
+
+        res.status(201).send(newProduct);
+    } catch (error) {
+        next(error);
+    }
+});
+
+// POST - Create a favorite product
+router.post("/users/:id/favorites", async (req, res, next) => {
+    try {
+        const user_id = req.params.id;
+        const { product_id } = req.body;
+
+        const favoriteProduct = await createFavorite({ user_id, product_id });
+
+        res.status(201).send(favoriteProduct);
+    } catch (error) {
+        next(error);
+    }
+});
+
+// Delete - Removes favorite products
+router.delete("/users/:userId/favorites/:id", async (req, res, next) => {
+    try {
+        const { userId, id } = req.params;
+
+        await destroyFavorite({ id, user_id: userId });
+
+        res.sendStatus(204);
+    } catch (error) {
+        next(error);
+    }
+});
 module.exports = router;
