1527085450f6f767710b67e62731178131d42751
Steve De La Rosa <stevedelarosa1036@gmail.com>
2024-03-09 19:37:12 -0500

    completed Database & Client

--- server/db.js
+++ server/db.js
@@ -1,4 +1,6 @@
 const pg = require("pg");
+const uuid = require("uuid");
+const bcrypt = require("bcrypt");
 
 const client = new pg.Client(
     process.env.DATABASE_URL || "postgres://localhost/the_acme_store"
@@ -32,4 +34,85 @@ const createTables = async () => {
     await client.query(SQL);
 };
 
-module.exports = { client, createTables };
+// creates a product in the database and returns the created record
+const createProduct = async ({ name }) => {
+    const SQL = `
+   INSERT INTO products(id, name) VALUES($1,$2) RETURNING *
+    `;
+    const response = await client.query(SQL, [uuid.v4(), name]);
+
+    return response.rows[0];
+};
+//  creates a user in the database and returns the created record
+const createUser = async ({ username, password }) => {
+    const SQL = `
+    INSERT INTO users(id, username, password) VALUES($1, $2, $3) RETURNING *   
+    `;
+
+    const response = await client.query(SQL, [
+        uuid.v4(),
+        username,
+        await bcrypt.hash(password, 5),
+    ]);
+    return response.rows[0];
+};
+// creates a favorite product in the database and return the created record
+const createFavorite = async ({ user_id, product_id }) => {
+    const SQL = `
+    INSERT INTO favorite_products(id, user_id, product_id) VALUES($1,$2,$3) RETURNING *
+    `;
+    const response = await client.query(SQL, [uuid.v4(), user_id, product_id]);
+
+    return response.rows[0];
+};
+
+// fetchUsers - fetches all the users
+const fetchUsers = async () => {
+    const SQL = `
+    SELECT * FROM users;
+    `;
+    const response = await client.query(SQL);
+    return response.rows;
+};
+// fetchProducts - fetches all the products
+const fetchProducts = async () => {
+    const SQL = `
+    SELECT * FROM products;
+    `;
+    const response = await client.query(SQL);
+
+    return response.rows;
+};
+
+// fetchFavorites - fetches all the favorite products
+const fetchFavorites = async () => {
+    const SQL = `
+    SELECT * FROM favorite_products;
+    `;
+
+    const response = await client.query(SQL);
+
+    return response.rows;
+};
+
+// destroyFavorite - removes favorite products
+
+const destroyFavorite = async ({ id, user_id }) => {
+    const SQL = `
+      DELETE FROM favorite_products
+      WHERE id = $1 AND user_id = $2
+    `;
+    await client.query(SQL, [id, user_id]);
+};
+
+module.exports = {
+    client,
+    createTables,
+    createProduct,
+    createUser,
+    createFavorite,
+    fetchUsers,
+    fetchProducts,
+    fetchFavorites,
+    destroyFavorite,
+};
--- server/index.js
+++ server/index.js
@@ -1,4 +1,14 @@
-const { client, createTables } = require("./db");
+const {
+    client,
+    createTables,
+    createProduct,
+    createUser,
+    fetchUsers,
+    fetchProducts,
+    fetchFavorites,
+    createFavorite,
+    destroyFavorite,
+} = require("./db");
 
 const express = require("express");
 const routes = require("./routes");
@@ -17,6 +27,32 @@ const init = async () => {
 
         await createTables();
         console.log("Tables Created 📊!");
+
+        const [moe, lucy, steve, tshirt, dress, perfume, jeans] =
+            await Promise.all([
+                createUser({ username: "moe", password: "password123" }),
+                createUser({ username: "lucy", password: "password123" }),
+                createUser({ username: "steve", password: "password123" }),
+
+                createProduct({ name: "t-shirt" }),
+                createProduct({ name: "dress" }),
+                createProduct({ name: "perfume" }),
+                createProduct({ name: "jeans" }),
+            ]);
+
+        const users = await fetchUsers();
+        console.log("My Users: ", users);
+
+        const products = await fetchProducts();
+        console.log("My Products: ", products);
+
+        const favoriteProduct = await Promise.all([
+            createFavorite({ user_id: steve.id, product_id: tshirt.id }),
+            createFavorite({ user_id: moe.id, product_id: jeans.id }),
+            createFavorite({ user_id: lucy.id, product_id: dress.id }),
+            createFavorite({ user_id: steve.id, product_id: perfume.id }),
+        ]);
+        console.log("My Favorites :", await fetchFavorites());
     } catch (error) {
         console.error(error);
     }
