cd38f37389a182b82a60cd70ba4bcc4dcfa4bc7f
Steve De La Rosa <stevedelarosa1036@gmail.com>
2024-03-09 19:44:49 -0500

    added user route

--- server/routes.js
+++ server/routes.js
@@ -1,4 +1,22 @@
 const express = require("express");
 const router = express.Router();
 
+const {
+    fetchUsers,
+    fetchProducts,
+    fetchFavorites,
+    createUser,
+    createProduct,
+    createFavorite,
+    destroyFavorite,
+} = require("./db");
+
+router.get("/users", async (req, res, next) => {
+    try {
+        res.send(await fetchUsers());
+    } catch (error) {
+        next(error);
+    }
+});
+
 module.exports = router;
